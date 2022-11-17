4fc1b71f6886a1e6659fbd7c0c735a898b4f4aeb
ricardofg2000 <ricardofg2000@gmail.com>
2022-11-17 12:03:31 +0100

    primera solución ejercicio 07

---
+++ src/unico/Ejercicio07.java
@@ -0,0 +1,41 @@
+package unico;
+
+public class Ejercicio07 {
+
+	public static void main(String[] args) {
+		/*
+		 * Implementar un método al que se le pase un número entero y devuelva el número
+		 * de divisores primos que tiene.
+		 */
+
+		int num;
+		int n_divP;
+
+		num = Util.leerInt("Introduzca un número: ");
+
+		n_divP = funcion1(num);
+		
+		Util.escribir("El número " + num + " tiene " + n_divP + " divisores primos");
+	}
+
+	public static int funcion1(int num) {
+		int cont = 0;
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				if (esPrimo(i)) {
+					cont += 1;
+				}
+			}
+		}
+		return cont;
+	}
+	static boolean esPrimo(int num) {
+		boolean primo = true;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				primo = false;
+			}
+		}
+		return primo;
+	}
+}
